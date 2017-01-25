d401924b324578884cee9c8f0ff4a0e2c5a86c4c
Joe Rinehart <rinehartjm09@gmail.com>
2017-01-24 19:25:52 -0800

    Added cancel button

--- app/src/main/java/com/jrinehartspokane/myapplication/MainActivity.java
+++ app/src/main/java/com/jrinehartspokane/myapplication/MainActivity.java
@@ -4,17 +4,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtLoginName;
+    EditText edtPassword;
+    CheckBox chkStayLoggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //change
+        edtLoginName = (EditText) findViewById(R.id.edtLoginName);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        chkStayLoggedIn = (CheckBox) findViewById(R.id.chkStayLoggedIn);
     }
 
     public void btnLoginOnClick(View v) {
-        Log.i("CLICK", "Login button was clicked");
+//        Log.i("CLICK", "Login button was clicked");
+//        Log.i( "CLICK", edtLoginName.getText().toString());
+        Toast.makeText(this, "You successfully logged in: " + edtLoginName.getText().toString() +
+                "\rYour Password is: " + edtPassword.getText().toString() +
+                "\r" + chkStayLoggedIn.isChecked(), Toast.LENGTH_LONG).show();
+    }
+
+    public void btnCancelOnClick(View v) {
+//        Log.i("CLICK", "Cancel was clicked");
+        edtLoginName.setText("");
+        edtPassword.setText("");
+        Toast.makeText(this, "Login has been canelled", Toast.LENGTH_LONG).show();
     }
 }
